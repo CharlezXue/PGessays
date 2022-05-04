7963d44a0689f5e8f034e1f62e8bd969bc6bea6c
CharlesXue <charlesxue96@gmail.com>
2022-05-04 20:13:11 +0800

    first build

--- essays_xml.py
+++ essays_xml.py
@@ -5,14 +5,19 @@ from lxml import etree
 source_rss = 'http://www.aaronsw.com/2002/feeds/pgessays.rss'
 local_rss = 'pgessays.rss'
 
+# url.urlretrieve(source_rss, local_rss)
+
 tree = ET.parse(local_rss)
 root = tree.getroot()
 
 # latest = root[0].find('item')[1].text
 # new = ET.fromstring(url.urlopen(source_rss, timeout=10).readlines())
 
+cnt = 0
 for item in root.iter('item'):
-    if item.find('description') is None:
+    cnt += 1
+    print(cnt)
+    if item.find('description') is None and item[0].text.endswith('html'):
         html_tree = etree.parse(item[0].text, etree.HTMLParser())
         for x in html_tree.xpath("//font"):
             x.attrib.clear()
