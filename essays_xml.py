a9468d81aa939629ebaf0e208a0baa6025bbdf88
CharlesXue <charlesxue96@gmail.com>
2022-05-04 22:26:33 +0800

    Add pubdate

--- essays_xml.py
+++ essays_xml.py
@@ -1,6 +1,6 @@
-import urllib.request as url
 import xml.etree.ElementTree as ET
 from lxml import etree
+from datetime import datetime
 
 source_rss = 'http://www.aaronsw.com/2002/feeds/pgessays.rss'
 local_rss = 'pgessays.rss'
@@ -13,6 +13,8 @@ root = tree.getroot()
 # latest = root[0].find('item')[1].text
 # new = ET.fromstring(url.urlopen(source_rss, timeout=10).readlines())
 
+
+
 cnt = 0
 for item in root.iter('item'):
     cnt += 1
@@ -24,4 +26,8 @@ for item in root.iter('item'):
         des = etree.tostring(html_tree.xpath("//font")[0])
         item.append(ET.Element('description'))
         item[2].text = des.decode()
+    if item.find('pubDate') is None:
+        item.append(ET.Element('pubDate'))
+        item[3].text = datetime.now().strftime("%a, %d %b %Y %X +0800")
+        # Sun, 04 Oct 2020 15:17:00 -0700
 tree.write(local_rss)
